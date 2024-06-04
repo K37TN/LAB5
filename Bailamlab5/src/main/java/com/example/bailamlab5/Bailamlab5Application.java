2d8b0aa34a39a69bc1f7738e84f5b2cf8a081cae
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-04 23:12:51 +0700

    .

---
+++ Bailamlab5/src/main/java/com/example/bailamlab5/Bailamlab5Application.java
@@ -0,0 +1,13 @@
+package com.example.bailamlab5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bailamlab5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bailamlab5Application.class, args);
+	}
+
+}
---
+++ Bailamlab5/src/test/java/com/example/bailamlab5/Bailamlab5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bailamlab5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bailamlab5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
