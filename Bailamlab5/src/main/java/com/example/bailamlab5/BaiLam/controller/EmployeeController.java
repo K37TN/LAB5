53fd445d51f15b9add30012cfdf060ca04609e05
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-05 08:41:15 +0700

    .

---
+++ Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/controller/EmployeeController.java
@@ -0,0 +1,4 @@
+package com.example.bailamlab5.BaiLam.controller;
+
+public class EmployeeController {
+}
---
+++ Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/entity/Employee.java
@@ -0,0 +1,4 @@
+package com.example.bailamlab5.BaiLam.entity;
+
+public class Employee {
+}
