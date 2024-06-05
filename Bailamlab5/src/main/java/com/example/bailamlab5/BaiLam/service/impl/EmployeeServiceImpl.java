b53845a4ad8c72e488418b542d18fbcfedbd238f
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-05 09:02:52 +0700

    .

--- Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/service/impl/EmployeeServiceImpl.java
+++ Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/service/impl/EmployeeServiceImpl.java
@@ -1,35 +1,39 @@
 package com.example.bailamlab5.BaiLam.service.impl;
 
 import com.example.bailamlab5.BaiLam.entity.Employee;
+import com.example.bailamlab5.BaiLam.repository.EmployeeRepository;
 import com.example.bailamlab5.BaiLam.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
+    @Autowired
+    EmployeeRepository employeeRepository;
     @Override
     public List<Employee> getAll() {
-        return null;
+        return employeeRepository.findAll();
     }
 
     @Override
     public void add(Employee employee) {
-
+employeeRepository.save(employee);
     }
 
     @Override
     public void delete(Integer id) {
-
+employeeRepository.deleteById(id);
     }
 
     @Override
     public Employee detail(Integer id) {
-        return null;
+        return employeeRepository.findById(id).get();
     }
 
     @Override
     public void update(Employee employee) {
-
+employeeRepository.save(employee);
     }
 }
