1762876884c59b2b38504e1305a40b0de5c82933
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-05 11:11:56 +0700

    Create lab5.sql

---
+++ lab5.sql
@@ -0,0 +1,26 @@
+﻿Create Database Lab5
+use Lab5
+go
+CREATE TABLE Department(
+	id int IDENTITY(1,1) PRIMARY KEY,
+	name nvarchar(50) NOT NULL,
+	address nvarchar(50),
+)
+
+CREATE TABLE Employee (
+	id int IDENTITY(1,1) PRIMARY KEY,
+	department_id int,
+	name nvarchar(50) NOT NULL,
+	age int,
+	address nvarchar(50),
+	birthday date,
+	salary float,
+	gender int,
+	FOREIGN KEY(department_id) REFERENCES Department(id)
+);
+INSERT INTO Department
+           (name ,address)
+VALUES
+           (N'Phòng Kinh doanh', N'Tố Hữu'),
+(N'Phòng Dịch vụ', N'Tô Hiệu'),
+(N'Phòng Hành chính', N'Nguyễn Phong Sắc');
