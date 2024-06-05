b869751d247831bc021f1409b5dda9018eb222fd
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-05 09:28:19 +0700

    .

--- Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/controller/EmployeeController.java
+++ Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/controller/EmployeeController.java
@@ -1,9 +1,22 @@
 package com.example.bailamlab5.BaiLam.controller;
 
+import com.example.bailamlab5.BaiLam.repository.EmployeeRepository;
+import com.example.bailamlab5.BaiLam.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/location")
 public class EmployeeController {
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping("/lost")
+    public String showAll(Model model){
+        model.addAttribute("líst",employeeService.getAll());
+        return "location/index";
+    }
 }
