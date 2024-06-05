535d9a88475b0e40fc3947316abad9e90a5e8656
Nguyễn Đức Mạnh <93751413+K37TN@users.noreply.github.com>
2024-06-05 08:52:44 +0700

    .

--- Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/entity/Employee.java
+++ Bailamlab5/src/main/java/com/example/bailamlab5/BaiLam/entity/Employee.java
@@ -1,4 +1,34 @@
 package com.example.bailamlab5.BaiLam.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Employee")
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "name")
+    private String  name;
+    @Column(name = "age")
+    private Integer age;
+    @Column(name = "bỉthday")
+    private LocalDate birthday;
+    @Column(name = "slady")
+    private Float slady;
+    @Column(name = "gender")
+    private Integer gender;
+    @ManyToOne
+    @JoinColumn(name = "department_id" , referencedColumnName = "id")
+    private  Department department;
 }
